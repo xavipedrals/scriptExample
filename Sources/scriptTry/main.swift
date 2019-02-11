478c79ad1af336b67e5e1e2319b06a7c951cfc85
Xavi Pedrals <xavi.pedrals@themobilecompany.com>
2019-02-11 17:15:19 +0100

    Minor changes

--- Sources/scriptTry/main.swift
+++ Sources/scriptTry/main.swift
@@ -1,9 +1,8 @@
 import Foundation
 import Files
+import ShellOut
 
 print("Hello, world!")
-//let folder = try Folder(path: Folder.current)
-let file = try Folder.current.file(named: "Concha.txt")
-let data = try file.read()
-print(String(data: data, encoding: .utf8))
-//try file.write(string: "{\"hello\": \"world\"}")
+
+let output = try shellOut(to: "touch deleteThisFile.stupid", at: "~/")
+print(output) // Hello world
